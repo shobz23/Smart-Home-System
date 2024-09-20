dd58e59efa2e3e7eae5541bb33ba7ad678648965
Shobitha V <143324729+shobz23@users.noreply.github.com>
2024-09-20 12:02:21 +0530

    Command.java

---
+++ Command.java
@@ -0,0 +1,44 @@
+public interface Command {
+    void execute();
+}
+
+// TurnOnCommand.java
+public class TurnOnCommand implements Command {
+    private Device device;
+
+    public TurnOnCommand(Device device) {
+        this.device = device;
+    }
+
+    @Override
+    public void execute() {
+        device.turnOn();
+    }
+}
+
+// TurnOffCommand.java
+public class TurnOffCommand implements Command {
+    private Device device;
+
+    public TurnOffCommand(Device device) {
+        this.device = device;
+    }
+
+    @Override
+    public void execute() {
+        device.turnOff();
+    }
+}
+
+// CommandInvoker.java
+public class CommandInvoker {
+    private Command command;
+
+    public void setCommand(Command command) {
+        this.command = command;
+    }
+
+    public void executeCommand() {
+        command.execute();
+    }
+}
